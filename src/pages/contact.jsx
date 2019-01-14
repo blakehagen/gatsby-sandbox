88ba324539131f6ea1b274f8c4450e5923a298a5
Blake Hagen <blakehagen@gmail.com>
2019-01-13 19:00:34 -0700

    react-helmet testing

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,11 +1,18 @@
-import React from "react"
-import { Link } from "gatsby"
-import Header from "../components/Header/header"
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { Link } from 'gatsby';
+import Header from '../components/Header/header';
 
 export default () => (
   <div style={{ color: `teal` }}>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Contact</title>
+      <meta name="description" content="Contact Bacon ipsum dolor amet ham hock tenderloin ham biltong sirloin burgdoggen spare ribs pastrami." />
+      <link rel="canonical" href="http://mysite.com/contact" />
+    </Helmet>
     <Link to="/">Home</Link>
-    <br/>
+    <br />
     <Link to="/about">About</Link>
     <Header headerText="Contact" />
     <p>Send us a message!</p>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,21 @@
-import React from "react";
-import { Link } from "gatsby";
-import Header from "../components/Header/header";
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { Link } from 'gatsby';
+import Header from '../components/Header/header';
 
 export default () => (
   <div style={{ color: `purple` }}>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>My Awesome Title</title>
+      <meta name="description" content="TEST PAGE Bacon ipsum dolor amet ham hock tenderloin ham biltong sirloin burgdoggen spare ribs pastrami." />
+      <link rel="canonical" href="http://mysite.com/example" />
+    </Helmet>
     <Link to="/about">About</Link>
-    <br/>
+    <br />
     <Link to="/contact/">Contact</Link>
     <Header headerText="Hello Gatsby!" />
     <p>What a world.</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
   </div>
-)
+);
