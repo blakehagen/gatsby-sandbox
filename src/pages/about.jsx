d03a8725b7d394acf6c0403556c3fcfa3ff64a8c
Blake Hagen <blakehagen@gmail.com>
2019-03-25 13:30:03 -0600

    training stuff

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import Header from "../components/Header/header";
+import { Link } from 'gatsby';
 
 export default () => (
   <div style={{ color: `teal` }}>
+    <Link to="/">Home</Link>
+    <br />
+    <Link to="/contact">Contact</Link>
     <Header headerText="About Gatsby" />
     <p>Such wow. Very React.</p>
   </div>
